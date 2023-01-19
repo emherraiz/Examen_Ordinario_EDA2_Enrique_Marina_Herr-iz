cadc08f55d9108f58520c215618df8276e39dda4
dmunofer <diego.fmz@hotmail.com>
2023-01-20 00:45:13 +0100

    cambios

---
+++ Ejercicio_3/test_artefactos_valiosos.py
@@ -0,0 +1,23 @@
+
+def test_crear_artefactos():
+    artefacto1 = ArtefactosValiosos(1, "Conserva de tomate", 2.5, "2022-12-31")
+    artefacto2 = ArtefactosValiosos(2, "Conserva de pimientos", 3.0, "2022-11-30")
+    artefacto3 = ArtefactosValiosos(3, "Conserva de alcachofas", 4.5, "2022-10-31")
+    assert artefacto1.nombre == "Conserva de tomate"
+    assert artefacto2.nombre == "Conserva de pimientos"
+    assert artefacto3.nombre == "Conserva de alcachofas"
+
+def test_orden_artefactos():
+    artefacto1 = ArtefactosValiosos(1, "Conserva de tomate", 2.5, "2022-12-31")
+    artefacto2 = ArtefactosValiosos(2, "Conserva de pimientos", 3.0, "2022-11-30")
+    artefacto3 = ArtefactosValiosos(3, "Conserva de alcachofas", 4.5, "2022-10-31")
+    artefactos_valiosos = [artefacto1, artefacto2, artefacto3]
+    artefactos_valiosos.sort(key=lambda x: x.fecha_caducidad)
+    assert artefactos_valiosos[0].fecha_caducidad == '2022-10-31'
+    assert artefactos_valiosos[1].fecha_caducidad == '2022-11-30'
+    assert artefactos_valiosos[2].fecha_caducidad == '2022-12-31'
+
+def test_modificar_precio():
+    artefacto1 = ej3.ArtefactosValiosos(1, "Conserva de tomate", 2.5, "2022-12-31")
+    artefacto1.precio = 2.0
+    assert artefacto1.precio == 2.0
