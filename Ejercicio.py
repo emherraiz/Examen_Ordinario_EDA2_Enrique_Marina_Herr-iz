639d55129d61e7c5b9956f4d44bda9d9e53bcae3
Enrique Marina <eherraiz@myuax.com>
2023-01-19 19:19:53 +0100

    supersayan

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,17 +1,32 @@
+import string, random
 class stormtrooper():
     def __init__(self, nombre, rango):
-        self.name = nombre
+        self.nombre = nombre
         self.rango = rango
         print('Stormtrooper se ha creado con éxito')
+        self.calificacion()
 
-    def calificacion(self, nombre):
-        temp = nombre.split('-')
+    def calificacion(self):
+        temp = self.nombre.split('-')
         self.codigo = temp[0]
-        temp = temp[1].split()
-        self.cohoerte = temp[0]
+        temp = temp[1]
+        self.cohorte = temp[0]
         self.siglo = temp[1]
         self.escuadra = temp[2]
         self.numero = temp[3]
 
     def __str__(self):
-        return 'Nombre: ' + self.name
+        return 'Nombre: ' + self.nombre + 'rango: ' + self.rango + 'codigo: ' + self.codigo + 'cohorte: ' + self.cohorte + 'siglo: ' + self.siglo + 'escuadra: ' + self.escuadra + 'numero: ' + self.numero
+
+lista_stormtrooper = []
+for i in range(4):
+    nombre = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + '-'
+    for j in range(4):
+        nombre += str(random.randint(0,9))
+
+
+    lista_stormtrooper.append(stormtrooper(nombre, 'Soldado'))
+
+for i in lista_stormtrooper:
+    print(i)
+
