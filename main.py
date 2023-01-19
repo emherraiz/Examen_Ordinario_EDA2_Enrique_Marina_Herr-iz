b805f5bda377ef9581c1db0bfc7ddc91c90fbdfb
Enrique Marina <eherraiz@myuax.com>
2023-01-19 23:20:31 +0100

    dd

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from Ejercicio_1 y Ejercicio_2
+from Ejercicio_1_y_2 import stormtrooper
+from Ejercicio_3 import artefactosvaliosos
+from Ejercicio_4 import usar_la_fuerza
+from Ejercicio_5 import ejercicio_5
+from Ejercicio_6 import ejercicio_6
+from Ejercicio_7 import ejercicio_7
+from Ejercicio_8 import ejercicio_8
+
 if __name__ == "__main__":
