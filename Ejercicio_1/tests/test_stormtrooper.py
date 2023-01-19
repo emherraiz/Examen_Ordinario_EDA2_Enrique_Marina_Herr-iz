a51d5ebf45bed3aab34c24063feceb4bd7593b42
dmunofer <diego.fmz@hotmail.com>
2023-01-19 22:55:16 +0100

    cambio

--- Ejercicio_1/tests/test_stormtrooper.py
+++ Ejercicio_1/tests/test_stormtrooper.py
@@ -1,11 +1,20 @@
-from Ejercicio_1 import Stormtrooper
-import pytest
+
+'''
 def test_calificacion():
     # Create a Stormtrooper object
     nombre = 'AB-1234'
-    stormtrooper = Stormtrooper(nombre, 'Soldado')
+    stormtrooper = stormtrooper.Stormtrooper(nombre, 'Soldado')
 
     # Test the calificacion method
     stormtrooper.calificacion()
     assert stormtrooper.codigo == 'AB'
-    assert stormtrooper.cohorte == '1'
+    assert stormtrooper.cohorte == '1'oooo
+'''
+
+def test_stormtrooper_calificacion():
+    stormtrooper = stormtrooper.Stormtrooper("TK-8654", "Capitán")
+    assert stormtrooper.calificacion() == "Codigo de legion: TK, Identificador de cohorte: 8, Identificador de siglo: 6, Identificador de escuadra: 5, Numero de trooper: 4"
+
+if __name__ == "main":
+    import pytest
+    pytest.main()
